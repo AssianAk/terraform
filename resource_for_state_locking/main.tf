341b37772dce4aee870fd1b43be5b761259f0b4b
Akash <ghangleakash0201@gmail.com>
2025-10-27 15:44:04 +0530

    new

---
+++ resource_for_state_locking/main.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "name" {
+    bucket = "mynewbucketforstatefile"
+}
+
+resource "aws_dynamodb_table" "dynamodb-for-state-file-lock" {
+  name = "terraform-state-lock-dyname"
+  hash_key = "akash"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "akash"
+    type = "s"
+  }
+}
